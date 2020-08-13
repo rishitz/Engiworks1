d0c67d28069674dc4efabec270c24f55c39b2b83
rishitz <rishitzaveri80@gmail.com>
2020-08-13 21:00:18 +0530

    janam2

--- build/generated-sources/ap-source-output/entity/Tblachievement_.java
+++ build/generated-sources/ap-source-output/entity/Tblachievement_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-12T14:15:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-13T18:05:04")
 @StaticMetamodel(Tblachievement.class)
 public class Tblachievement_ { 
 
