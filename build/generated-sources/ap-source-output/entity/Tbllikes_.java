3edbb5637b00f60e8f9f2d6b289956ce0896c2f0
rishitz <rishitzaveri80@gmail.com>
2020-08-15 01:30:40 +0530

    modal

--- build/generated-sources/ap-source-output/entity/Tbllikes_.java
+++ build/generated-sources/ap-source-output/entity/Tbllikes_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-14T15:22:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-08-14T23:43:03")
 @StaticMetamodel(Tbllikes.class)
 public class Tbllikes_ { 
 
